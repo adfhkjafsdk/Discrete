3218b9bfb3bc6e908b8291f452807cfa2027cc3e
rzh123 <rzh12345678900@outlook.com>
2025-09-29 19:52:08 +0800

    Update test.lean.

--- test.lean
+++ test.lean
@@ -33,3 +33,22 @@ example (P : Prop) : P ∨ ¬P ↔ True := by
   · -- Assume True, prove P ∨ ¬P
     intro h
     exact h1
+
+example (P : Prop) :
+  (¬P)↔(P→False) := by
+  apply Iff.intro
+  · -- Assume ¬P, prove P→False
+    intro h1 h2
+    contradiction
+  · -- Assume P→False, prove ¬P
+    intro h1 h2
+    apply h1
+    exact h2
+
+
+lemma iff_not_self_iff_false (P : Prop) :
+  (P↔¬P)↔False := by
+  rw [@iff_iff_implies_and_implies P (¬P)]
+  nth_rw 4 [← @not_not P]
+  repeat rw [imp_not_self]
+  rw [and_not_self_iff]
